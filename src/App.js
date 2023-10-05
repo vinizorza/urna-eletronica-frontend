035fbb8a2ad3f30d290b22e41cb50229749b6fde
Vinicius Zorzanelli <vinizorza@hotmail.com>
2023-10-05 23:03:50 +0100

    feat: Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+function App() {
+  return (
+    <div className="container">
+
+      <div className="display">
+
+      </div>
+      <div className="rightSide">
+        <div className="title">JUSTIÇA ELEITORAL</div>
+        <div className="keyboard">
+          <button className="numberButton">
+            <p className="buttonNumberText">1</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">2</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">3</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">4</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">5</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">6</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">7</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">8</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">9</p>
+          </button>
+          <button className="numberButton">
+            <p className="buttonNumberText">0</p>
+          </button>
+        </div>
+        <div className="actionButtons">
+          <button className="whiteButton">
+            <p className="buttonActionText">BRANCO</p>
+          </button>
+          <button className="redButton">
+            <p className="buttonActionText">CORRIGE</p>
+          </button>
+          <button className="greenButton">
+            <p className="buttonActionText">CONFIRMA</p>
+          </button>
+        </div>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
